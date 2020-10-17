d8dd899b3b7745867ca03f18c30a29c50bc57030
elsadwi17 <56186516+elsadwi17@users.noreply.github.com>
2020-10-18 00:49:32 +0700

    bismillah mulai

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/iLinkxyz/';
+$config['base_url'] = 'http://localhost/ilink/';
 
 /*
 |--------------------------------------------------------------------------
