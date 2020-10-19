f25340b64c972614a8a6bb6a7dec996702bf205c
elsadwi17 <56186516+elsadwi17@users.noreply.github.com>
2020-10-20 01:20:29 +0700

    security output part 4

--- application/views/tampil.php
+++ application/views/tampil.php
@@ -12,16 +12,16 @@
                 echo '<h1>MY LINK</h1>'; ?>
                 <div class='bingkai-foto'>
                     <?php $user = $this->db->get_where('user', ['username' => $username])->row_array(); ?>
-                    <img class='foto-profile' src='<?= base_url('assets/img/profile/') . $user['image'] ?>'>
+                    <img class='foto-profile' src='<?= esc_url (base_url('assets/img/profile/') . $user['image']) ?>'>
                 </div>
                 <div class="link">
                 <?php
-                    echo '<a href="#">ilinkxyz.com/' . $username . '</a>';
+                    echo esc_url ('<a href="#">ilinkxyz.com/' . $username . '</a>');
                     foreach ($query->result() as $row) {
                         echo '
                                 <div class="link-btn">';
                         if (substr($row->link, 0, 4) == 'http') {
-                            echo '<a class="btn btn-primary btn-tampil" target="_blank" href="' . $row->link . '">' . $row->title . '</a>';
+                            echo esc_url ('<a class="btn btn-primary btn-tampil" target="_blank" href="' . $row->link . '">' . $row->title . '</a>');
                         } else {
                             echo esc_url('<a class="btn btn-primary btn-tampil" target="_blank" href="https://' . $row->link . '">' . $row->title . '</a>');
                         }
